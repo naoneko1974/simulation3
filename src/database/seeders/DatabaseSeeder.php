f3ae96f3b048dc1b5200c05be5a3055e0944897b
nagaoka naoki <naoneko1974@gmail.com>
2024-01-10 10:32:04 +0900

    フリマアプリ

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'manager',
+            'email' => 'manager@manage.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'authority' => 1,
+            'introduction' => 1,
+            'staff' => 0,
+        ]);
+
+        DB::table('profiles')->insert([
+            'user_id' => 1,
+        ]);
+        
         $this->call(ConditionsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(PaymentsTableSeeder::class);
